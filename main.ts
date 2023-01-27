3c4a407625e7c879922ff2389c073ba57d3b1ecf
git4o <71034923+git4o@users.noreply.github.com>
2023-01-26 19:46:27 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+WSJoyStick.onKey(KEY.F, function () {
+    radio.sendNumber(7)
+})
+WSJoyStick.onKey(KEY.A, function () {
+    radio.sendNumber(2)
+})
+WSJoyStick.onKey(KEY.P, function () {
+    radio.sendNumber(1)
+})
+WSJoyStick.onKey(KEY.E, function () {
+    radio.sendNumber(6)
+})
+WSJoyStick.onKey(KEY.D, function () {
+    radio.sendNumber(5)
+})
+WSJoyStick.onKey(KEY.B, function () {
+    radio.sendNumber(3)
+})
+WSJoyStick.onKey(KEY.C, function () {
+    radio.sendNumber(4)
+})
+WSJoyStick.JoyStickInit()
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
