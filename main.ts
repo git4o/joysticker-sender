ad62b6c44184bbdb82c65c7b554a4dd7584feb0d
git4o <71034923+git4o@users.noreply.github.com>
2023-01-27 15:36:11 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 WSJoyStick.onKey(KEY.F, function () {
     radio.sendNumber(7)
 })
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.sendNumber(17)
+})
 WSJoyStick.onKey(KEY.A, function () {
     radio.sendNumber(2)
 })
@@ -19,8 +22,28 @@ WSJoyStick.onKey(KEY.B, function () {
 WSJoyStick.onKey(KEY.C, function () {
     radio.sendNumber(4)
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(16)
+})
 WSJoyStick.JoyStickInit()
 radio.setGroup(1)
 basic.forever(function () {
-	
+    if (WSJoyStick.Listen_Dir(DIR.U)) {
+        radio.sendNumber(8)
+    } else if (WSJoyStick.Listen_Dir(DIR.D)) {
+        radio.sendNumber(9)
+    } else if (WSJoyStick.Listen_Dir(DIR.L)) {
+        radio.sendNumber(10)
+    } else if (WSJoyStick.Listen_Dir(DIR.R)) {
+        radio.sendNumber(11)
+    }
+    if (WSJoyStick.Listen_Dir(DIR.U_L)) {
+        radio.sendNumber(12)
+    } else if (WSJoyStick.Listen_Dir(DIR.U_R)) {
+        radio.sendNumber(13)
+    } else if (WSJoyStick.Listen_Dir(DIR.D_L)) {
+        radio.sendNumber(14)
+    } else if (WSJoyStick.Listen_Dir(DIR.D_R)) {
+        radio.sendNumber(15)
+    }
 })
